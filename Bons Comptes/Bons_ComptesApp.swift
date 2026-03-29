f29a3835314fe899320ef6fe25b1529c174d1e1d
BudgetApp <bob.oulhen@gmail.com>
2026-03-29 11:54:03 +0200

    Initial Commit

---
+++ Bons Comptes/Bons_ComptesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bons_ComptesApp.swift
+//  Bons Comptes
+//
+//  Created by Robert Oulhen on 29/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Bons_ComptesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
